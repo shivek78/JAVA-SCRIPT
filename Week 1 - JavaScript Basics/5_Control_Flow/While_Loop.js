afc742db3632bb301805bd2abdfc2b05b12a5040
shivek yadav <shivekyadav0786@gmail.com>
2024-10-17 15:54:21 +0530

    for loop

---
+++ Week 1 - JavaScript Basics/5_Control_Flow/While_Loop.js
@@ -0,0 +1,109 @@
+const { lookup } = require("dns");
+
+// Display the header
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+//typ of loop in js 
+//for loop 
+//forin loop 
+//forof look
+//while look
+//do while loop
+//example
+let a=1;
+for(let i=0;i<100;i++){
+      console.log(i)
+
+};
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Sum of Numbers                                                    +");
+console.log("+  Write a program to calculate the sum of numbers from 1 to 100 using a for loop.          +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+var sum=0;
+for(i=1;i<=100;i++){
+      var sum=sum+i;
+      console.log(sum)
+}
+
+
+
+
+
+
+
+
+
+
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+          Factorial of a Number                                                           +");
+console.log("+  Create a program that calculates the factorial of a given number using a for loop.      +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+
+let n = 5;
+let result = 1; 
+if (n !== 0) {
+    for (let i = 1; i <= n; i++) { 
+        result *= i;
+    }
+    console.log(result);  // Output the factorial
+} else {
+    console.log(1);  // Factorial of 0 is 1
+}
+
+
+/*
+
+
+console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");console.log("+----+--------+----------------+----------------------+------------------------------------+");
+console.log("+                                                                                           +");
+console.log("+                         Example of using loops in JavaScript:                           +");
+console.log("+                                                                                           +");
+console.log("+----+--------+----------------+----------------------+------------------------------------+");*/
